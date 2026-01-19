829b675b3f405e6a058939304b8d3f44cfd5004a
Faria Kabir <fariakabir@Farias-MacBook-Air.local>
2026-01-19 14:39:56 -0500

    Fix: Explicitly set NextAuth secret

--- calendar-booking/src/lib/auth.ts
+++ calendar-booking/src/lib/auth.ts
@@ -23,6 +23,7 @@ declare module 'next-auth/jwt' {
 }
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma) as NextAuthOptions['adapter'],
   providers: [
     GoogleProvider({
